398ebfc760cdf0271ca25ea09126cd0934833466
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2022-05-09 23:56:41 +0900

    Update StringRepeat.js
    수정 사항 반영

--- src/backjoon/level6/StringRepeat.js
+++ src/backjoon/level6/StringRepeat.js
@@ -1,33 +1,34 @@
 /*
-문제 링크
-https://www.acmicpc.net/problem/2675
+[문제]
+문자열 S를 입력받은 후에, 각 문자를 R번 반복해 새 문자열 P를 만든 후 출력하는 프로그램을 작성하시오. 즉, 첫 번째 문자를 R번 반복하고, 
+두 번째 문자를 R번 반복하는 식으로 P를 만들면 된다. S에는 QR Code "alphanumeric" 문자만 들어있다.
+
+입력으로는
+첫째 줄에 테스트 케이스의 개수 T(1 ≤ T ≤ 1,000)가 주어진다. 각 테스트 케이스는 반복 횟수 R(1 ≤ R ≤ 8), 
+문자열 S가 공백으로 구분되어 주어진다. S의 길이는 적어도 1이며, 20글자를 넘지 않는다. 
+
+입력 예시)
+2
+3 ABC
+5 /HTP
+
+출력 예시) 
+AAABBBCCC
+/////HHHHHTTTTTPPPPP
 * */
-//const input = require('../../helper/inputHelper');
-
-/*
-acc ?
-initialValue(Optional)
-최초 callback함수 실행 시 accumulator 인수에 제공되는 값,
-초기값을 제공하지 않을경우 배열의 첫 번째 요소를 사용하고,
-빈 배열에서 초기값이 없을 경우 에러가 발생합니다.
-
-currentValue - 배열의 현재 요소
-index - 배열의 현재 요소 인덱스
-array - 호출한 배열
-*/
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/input.txt';
-let input = fs.readFileSync(filePath).toString().trim().split('\n');
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 input.shift();
 
-input.map((acc) => {
-    const word = acc.split(" ");
-    var alphabet = word[1].split("");
+input.forEach((acc) => {
+    const countAndWord = acc.split(" ");
+    const alphabet = countAndWord[1].split("");
     var result = "";
 
-    alphabet.map((str) => {
-        result += str.repeat(word[0]);
+    alphabet.forEach((str) => {
+        result += str.repeat(countAndWord[0]);
     })
 
     console.log(result);
